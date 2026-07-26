41e92b77c28c160105e562ee9d7b0a88e7548a06
jacksonvin <37443926+jacksonvin@users.noreply.github.com>
2026-07-26 21:36:01 +0800

    Update output.js

--- output.js
+++ output.js
@@ -1,6 +1,3 @@
-//Sun Jul 26 2026 13:35:11 GMT+0000 (Coordinated Universal Time)
-//Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
-//Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
 const $ = new Env('捕鱼助力');
 function OBHae8I() {}
 var OgVzS6j = Object['defineProperty'],
